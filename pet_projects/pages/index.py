8842f387538e01b435137c6f25c3872fa44d3e8a
inrezy <xardmax2000@gmail.com>
2026-04-19 02:52:28 +0700

    changed description on index page

--- pet_projects/pages/index.py
+++ pet_projects/pages/index.py
@@ -5,7 +5,16 @@ from ..templates import general_template
 
 @general_template(route="/", title="Index Page")
 def index() -> rx.Component:
-    return rx.flex(
-        rx.text("sex porno"),
-        justify='center'
+    return rx.vstack(
+        rx.text(
+            "Hi! Welcome to Pet Projects Collection website.",
+            size='8',
+            weight='medium'
+        ),
+        rx.text(
+            "This website contains some interesting pet projects (I think), so enjoy lol.",
+            size='6',
+            weight='light'
+        ),
+        align='center'
     )
